6f87acd38258f3e5ca6d01eb71afb021d411f9a1
Huajian Mao <huajianmao@gmail.com>
2012-02-10 12:44:37 +0800

    Notification Supported 	modified:   src/cn/hjmao/msgfilter/SmsReceiver.java

--- src/cn/hjmao/msgfilter/SmsReceiver.java
+++ src/cn/hjmao/msgfilter/SmsReceiver.java
@@ -1,5 +1,8 @@
 package cn.hjmao.msgfilter;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.ContentValues;
 import android.content.Context;
@@ -10,6 +13,8 @@ import android.util.Log;
 
 public class SmsReceiver extends BroadcastReceiver {
 
+	private Context context;
+	
 	private RuleManager ruleManager;
 	private static Uri SMSINBOX_URI = Uri.parse("content://sms/inbox");
 	public SmsReceiver() {
@@ -22,6 +27,7 @@ public class SmsReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		Log.v("TAG", "onReceive");
 
+		this.context = context;
 		Object[] pdus = (Object[]) intent.getExtras().get("pdus");
 		if (pdus != null && pdus.length > 0) {
 			SmsMessage[] messages = new SmsMessage[pdus.length];
@@ -47,6 +53,7 @@ public class SmsReceiver extends BroadcastReceiver {
 						e.printStackTrace();
 					}
 					this.abortBroadcast();
+					notification(content);
 				}
 			}
 		}
@@ -60,5 +67,20 @@ public class SmsReceiver extends BroadcastReceiver {
 		values.put( "type", 1);
 		values.put("body", content);
 		return values;
+	}
+	
+	private void notification(String content) {
+		NotificationManager notifMng = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		Notification notif = new Notification();
+		notif.icon = android.R.drawable.stat_notify_chat;
+		notif.tickerText = "New msgfilter notification";
+		notif.defaults = Notification.DEFAULT_SOUND;
+		notif.flags |= Notification.FLAG_AUTO_CANCEL;
+		notif.flags |= Notification.DEFAULT_SOUND;
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setType("vnd.android-dir/mms-sms");
+		PendingIntent pIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+		notif.setLatestEventInfo(context, "MsgFilter", "You have unread mail notifcation!", pIntent);
+		notifMng.notify(0, notif);
 	}
 }
